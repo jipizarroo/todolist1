2daa162b751cdb4fceefdc4528808f3c6c457697
jipizarroo <jipizarroo@gmail.com>
2020-01-08 15:30:22 -0300

    todolist terminado

--- src/App.js
+++ src/App.js
@@ -1,26 +1,73 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            estado: 1, 
+            mostrar: "d-flex",
+            esconder: "d-none",
+            notas: []
+        }
+
+        this.getValor = this.getValor.bind(this);
+    }
+
+    getValor(e) {
+        if (e.keyCode === 13 && e.target.value !== '') {
+            this.setState({
+                notas: this.state.notas.concat(e.target.value),
+                estado: 2
+            })
+
+        }
+    }
+
+    sendToTrash(y){
+        const { notas } = this.state;
+        notas.splice(y, 1)
+        this.setState({
+            notas: notas
+        })
+
+    }
+
+    render() {
+        return (
+            <>
+                <div className="container" id="body">
+                    <div className="row">
+                        <div className="col d-flex justify-content-center" id="titulo">
+                            <h1>HOLA MUNDO</h1>
+                        </div>
+                    </div>
+
+                    <div className="row d-flex justify-content-center">
+                        <div className="col-7" id="cuerpotarjeta">
+                            <input type="text" onKeyDown={(e) => this.getValor(e)} />
+                            <ul className="list-group-flush border-bottom border-top">
+                                <li className={`list-group-item justify-content-between mt-2 text-danger ${this.state.estado === 1 ? this.state.mostrar : this.state.esconder}`}>No tasks, add a task</li>
+                                {
+                                    this.state.notas.length > 0 &&
+                                    this.state.notas.map((nota, i) => {
+                                        return (
+
+                                            <li className="list-group-item d-flex justify-content-between mt-2"key={i}>
+                                                {nota} <i className="fas fa-trash" id="basurita" onClick={(y) => this.sendToTrash(i)}></i>
+                                            </li>
+                                        )
+                                    }
+                                    )
+                                }
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </>
+        )
+    }
 }
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+// Add css files
+import 'bootstrap/dist/css/bootstrap.min.css';
+//import 'font-awesome/css/font-awesome.min.css'; // 4.x
+import '@fortawesome/fontawesome-free/css/all.css'; // 5.x
+// Add js files
+import 'jquery';
+import 'popper.js';
+import 'bootstrap';
+import './index.css';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
